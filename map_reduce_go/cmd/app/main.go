e1a2c882e8ed9d93b391b671615901289e9912b9
Rishikesh Ramjith <kesh.ramjith@protonmail.com>
2024-10-26 11:55:33 +0200

    show filenames of input dir

--- map_reduce_go/cmd/app/main.go
+++ map_reduce_go/cmd/app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	args := os.Args[1:]
+	if len(args) < 1 {
+		panic("input files required.")
+	}
+	dirEntries, err := os.ReadDir(args[0])
+	if err != nil {
+		panic(err)
+	}
+
+	for _, dirEntry := range dirEntries {
+		fmt.Println(dirEntry.Name())
+	}
 }
